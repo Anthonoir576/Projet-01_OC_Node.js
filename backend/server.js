6ed72691293f6efd0d91c37188eaa4801750e33b
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-20 11:52:33 +0200

    mon premier serveur

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+/* IMPORTATION DU PACKAGE HTTP DE NODE */
+/* Maintenant on a acces a cette objet http, qui nous permet de créer un serveur */
+const http = require('http');
+
+/* Création du SERVEUR */
+/* On appel la methode createServer() du package http */
+/* Prend en argument 2 choses, la requète et la réponse */
+const server = http.createServer((request, response) => {
+
+    response.end('Reponse serveur SECONDE is here');
+
+});
+/* Serveur prêt */
+
+
+/* Maintenant le serveur doit écouter / Attendre les requètes envoyés  */
+/* en première argument : si l'environnement tourne votre serveur vous envoie un port a utilisé, en deuxième arguments par defaut en 3000 */
+server.listen(process.env.PORT || 3000);
