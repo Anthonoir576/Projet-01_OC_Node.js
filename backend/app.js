aa5148319f29cea7d0c5b58e3fb1722352c74494
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-20 13:46:28 +0200

    config express app.js => server.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 
+/* *** 01 *** */
 /* Importation de express */
 const express = require('express');
 
+
+/* *** 02 *** */
 /* Notre application */
 const app = express();
 
+
+/* *** 03 *** */
+app.use((request, response) => {
+
+    response.json( {message : 'Votre requête a bien été reçue'});
+
+});
+
+
+/* *** 04 *** */
 /* On exporte la constante app afin d'y accéder depuis les autres fichiers de notre projet notamment node */
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
+
 /* IMPORTATION DU PACKAGE HTTP DE NODE */
 /* Maintenant on a acces a cette objet http, qui nous permet de créer un serveur */
+/* *** 01 *** */
 const http = require('http');
+
+
+/* *** 02 *** */
+/* On récupère express, configurer dans le fichier app.js */
+const app = require('./app');
 
 /* Création du SERVEUR */
 /* On appel la methode createServer() du package http */
-/* Prend une fonction en 2 argument, la requète et la réponse */
-const server = http.createServer((request, response) => {
+/* Prend une fonction en 2 argument, la requête et la réponse */
+// const server = http.createServer((request, response) => {
 
-    response.end('Reponse serveur SECONDE is here');
+//     response.end('Reponse serveur');
 
-});
+// });
 /* Serveur prêt */
 
 
-/* Maintenant le serveur doit écouter / Attendre les requètes envoyés  */
-/* en première argument : si l'environnement tourne votre serveur vous envoie un port a utilisé, en deuxième arguments par defaut en 3000 */
+/* *** 03 *** */
+/* On dit a l'application express sur quel port elle va tourner. Exactement comme plus bas. */
+app.set('port', process.env.PORT || 3000);
+
+/* l'application créer par express, et une fonction. Qu'on a récupéré via l'importe plus haut */
+const server = http.createServer(app);
+
+
+/* *** 04 *** */
+/* Maintenant le serveur doit écouter / Attendre les requêtes envoyés  */
+/* en première argument : si l'environnement retourne un serveur vous envoie un port a utilisé, en deuxième arguments par defaut en 3000 */
 server.listen(process.env.PORT || 3000);
