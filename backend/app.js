d200573e591332115820147a1ed8e8a6e9defc44
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-22 12:42:07 +0200

    config url pour methode get => pour le front

--- backend/app.js
+++ backend/app.js
@@ -13,32 +13,28 @@ const app = express();
 /* ###### MIDDLEWARE ###### */
 /* Réponse pour le moment pour tous type de requete effectuer sur celle-ci */
 /* 1ere argument la requete, le 2eme la reponse, la 3eme next, qui permet de passé au prochain middleware .*/
-app.use((request, response, next) => {
 
-    console.log('1ere requête');
-    next();
-
-});
-
-/* status 201 created, et le changement de statue de la requete */
-app.use((request, response, next) => {
-
-    response.status(201);
-    next();
-
-});
-
-app.use((request, response, next) => {
-
-    response.json( {message : 'Votre requête a bien été reçue'});
-    next();
-
-});
-
-app.use((request, response) => {
-
-    console.log('Réponse envoyée avec succès');
-
+/* /api/stuff serra la route de l'api, l'url de l'appli front va faire une requete a cette url la . a sont extention. il renverra un code 200 si la reponse est reussite en json de l'array stuff */
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
 });
 
 
