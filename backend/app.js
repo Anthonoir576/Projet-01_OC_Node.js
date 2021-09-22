86d93f46535fa86cefabec9e632aa05eded55250
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-22 08:11:21 +0200

    mise en place des middleware exemple.+ changement server avec ajout fonction de controle port, et gestion derreur

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,31 @@ const app = express();
 
 /* *** 03 *** */
 /* Réponse pour le moment pour tous type de requete effectuer sur celle-ci */
-app.use((request, response) => {
+/* 1ere argument la requete, le 2eme la reponse, la 3eme next, qui permet de passé au prochain middleware .*/
+app.use((request, response, next) => {
+
+    console.log('1ere requête');
+    next();
+
+});
+/* status 201 created, et le changement de statue de la requete */
+app.use((request, response, next) => {
+
+    response.status(201);
+    next();
+
+});
+
+app.use((request, response, next) => {
 
     response.json( {message : 'Votre requête a bien été reçue'});
+    next();
+
+});
+
+app.use((request, response) => {
+
+    console.log('Réponse envoyée avec succès');
 
 });
 
--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ const http = require('http');
 const app = require('./app');
 
 /* Création du SERVEUR */
-/* On appel la methode createServer() du package http */
-/* Prend une fonction en 2 argument, la requête et la réponse */
+/* On appel la methode createServer() du package http . Prend une fonction en 2 argument, la requête et la réponse */
 // const server = http.createServer((request, response) => {
 
 //     response.end('Reponse serveur');
@@ -19,16 +18,61 @@ const app = require('./app');
 // });
 /* Serveur prêt */
 
-
 /* *** 03 *** */
-/* On dit a l'application express sur quel port elle va tourner. Exactement comme plus bas. */
-app.set('port', process.env.PORT || 3000);
+/* la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne */
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+};
 
+/* *** 04 *** */
+/* On dit a l'application express sur quel port elle va tourner. Exactement comme plus bas. */
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/* *** 05 *** */
+/* la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur */
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+};
+
+/* *** 05 *** */
 /* l'application créer par express, et une fonction. Qu'on a récupéré via l'importe plus haut */
 const server = http.createServer(app);
 
+/* *** 06 *** */
+/* un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console */
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
 
-/* *** 04 *** */
+/* *** 07 *** */
 /* Maintenant le serveur doit écouter / Attendre les requêtes envoyés  */
 /* en première argument : si l'environnement retourne un serveur vous envoie un port a utilisé, en deuxième arguments par defaut en 3000 */
-server.listen(process.env.PORT || 3000);
+server.listen(port);
